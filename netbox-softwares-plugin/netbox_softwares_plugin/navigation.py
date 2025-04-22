13639c8185a78b438d99548790ad0ae9d74d8c1f
AnttiV <antti.vesanto@verkkokauppa.com>
2025-04-22 13:46:15 +0300

    fix menu_items

--- netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
+++ netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
@@ -6,6 +6,5 @@ menu_items = (
         link_text='My Link',         # Label in the menu
         buttons=[],                  # No buttons needed
         permissions=[],              # Leave blank for public access
-        icon_class='mdi mdi-link',   # Material Design Icon (optional)
     ),
 )
