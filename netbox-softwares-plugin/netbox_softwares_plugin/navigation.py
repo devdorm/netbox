645a80622fcebd32cabee3e684551e6182e9c3cf
AnttiV <antti.vesanto@verkkokauppa.com>
2025-04-22 14:44:47 +0300

    modify navigation

--- netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
+++ netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
@@ -9,23 +9,17 @@ from netbox.plugins import PluginMenuItem, PluginMenu
     ),
 )'''
 
-item1 = PluginMenuItem(
-        link='plugins:netbox_softwares_plugin:softwares_list',  # 
-        link_text='My Link',         # Label in the menu
-        buttons=(),                  # No buttons needed
-        permissions=[],              # Leave blank for public access
-    )
-item2 = PluginMenuItem(
-        link='plugins:netbox_softwares_plugin:softwares_list',  # 
-        link_text='My Link',         # Label in the menu
-        buttons=(),                  # No buttons needed
-        permissions=[],              # Leave blank for public access
+list = PluginMenuItem(
+        link='plugins:netbox_softwares_plugin:softwares_list',
+        link_text='Softwares',
+        buttons=(),
+        permissions=[],
     )
 
 menu = PluginMenu(
     label='Softwares',
     groups=(
-        ('My Link',(item1, item2)),
+        ('Installed Softwares',(list,)),
     ),
     icon_class='mdi mdi-developer-board'
 )
