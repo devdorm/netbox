3f0e3bb0d3420557a067543a8cfb2f3bf2d78a2d
AnttiV <antti.vesanto@verkkokauppa.com>
2025-04-22 13:56:56 +0300

    add menu

--- netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
+++ netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
@@ -1,10 +1,30 @@
-from netbox.plugins import PluginMenuItem
+from netbox.plugins import PluginMenuItem, PluginMenu
 
 menu_items = (
     PluginMenuItem(
         link='plugins:netbox_softwares_plugin:softwares_list',  # 
         link_text='My Link',         # Label in the menu
-        buttons=[],                  # No buttons needed
+        buttons=(),                  # No buttons needed
         permissions=[],              # Leave blank for public access
+    ),
+)
+
+item1 = PluginMenuItem(
+        link='plugins:netbox_softwares_plugin:softwares_list',  # 
+        link_text='My Link',         # Label in the menu
+        buttons=(),                  # No buttons needed
+        permissions=[],              # Leave blank for public access
+    )
+item2 = PluginMenuItem(
+        link='plugins:netbox_softwares_plugin:softwares_list',  # 
+        link_text='My Link',         # Label in the menu
+        buttons=(),                  # No buttons needed
+        permissions=[],              # Leave blank for public access
+    )
+
+menu = PluginMenu(
+    label='Softwares',
+    groups=(
+        ('My Link',(item1, item2))
     ),
 )
