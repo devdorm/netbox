9222ead811b624d2404b70eb3ae962590a3336c7
AnttiV <antti.vesanto@verkkokauppa.com>
2025-04-22 13:50:26 +0300

    fix link

--- netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
+++ netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
@@ -2,7 +2,7 @@ from netbox.plugins import PluginMenuItem
 
 menu_items = (
     PluginMenuItem(
-        link='',  # External or internal link
+        link='plugins:netbox_softwares_plugin:softwares_list',  # 
         link_text='My Link',         # Label in the menu
         buttons=[],                  # No buttons needed
         permissions=[],              # Leave blank for public access
