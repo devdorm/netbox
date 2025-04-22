9ffb24485da3f6fbfb79f62c3c60692ce4670fc3
AnttiV <antti.vesanto@verkkokauppa.com>
2025-04-22 11:17:43 +0300

    fix navigation

--- netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
+++ netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
@@ -1,4 +1,4 @@
-from extras.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
+from extras.plugins import PluginMenuButton, PluginMenuItem
 from utilities.choices import ButtonColorChoices
 
 plugin_buttons = [
@@ -9,21 +9,22 @@ plugin_buttons = [
     )
 ]
 
+# Plugin submenu
+
+menu = PluginMenu(
+    label="Softwares",
+    groups=(
+        ('Foo', (item1)),
+    ),
+    icon_class='"mdi mdi-apps-box'
+)
+
 # Plugin submenu items
 
 item1 = (
     PluginMenuItem(
         link="plugins:netbox_softwares_plugin:softwares_list",
         link_text="Softwares",
+        buttons=plugin_buttons,
     ),
-)
-
-# Plugin submenu
-
-menu = PluginMenu(
-    label="Softwares",
-    groups=(
-        ("Foo", (item1,)),
-    ),
-    icon_class="mdi mdi-apps-box"
 )
