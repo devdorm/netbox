e261c815b2f763ff8259a53a76196f453378087e
AnttiV <antti.vesanto@verkkokauppa.com>
2025-04-22 13:44:15 +0300

    navigation test

--- netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
+++ netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
@@ -1,18 +1,11 @@
-from extras.plugins import PluginMenuButton, PluginMenuItem
-from utilities.choices import ButtonColorChoices
-
-plugin_buttons = [
-    PluginMenuButton(
-        link="plugins:netbox_softwares_plugin:softwares_add",
-        title="Add",
-        icon_class="mdi mdi-plus-thick",
-    )
-]
+from netbox.plugins import PluginMenuItem
 
 menu_items = (
     PluginMenuItem(
-        link="plugins:netbox_softwares_plugin:softwares_list",
-        link_text="Softwares",
-        buttons=plugin_buttons,
+        link='https://example.com',  # External or internal link
+        link_text='My Link',         # Label in the menu
+        buttons=[],                  # No buttons needed
+        permissions=[],              # Leave blank for public access
+        icon_class='mdi mdi-link',   # Material Design Icon (optional)
     ),
 )
