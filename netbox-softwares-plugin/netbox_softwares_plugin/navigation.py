58aacee9be575afd95de4c4959e0ad0089803e16
AnttiV <antti.vesanto@verkkokauppa.com>
2025-04-22 13:01:55 +0300

    back to default

--- netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
+++ netbox-softwares-plugin/netbox_softwares_plugin/navigation.py
@@ -1,4 +1,4 @@
-from extras.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
+from extras.plugins import PluginMenuButton, PluginMenuItem
 from utilities.choices import ButtonColorChoices
 
 plugin_buttons = [
@@ -9,22 +9,10 @@ plugin_buttons = [
     )
 ]
 
-# Plugin submenu items
-
-item1 = (
+menu_items = (
     PluginMenuItem(
         link="plugins:netbox_softwares_plugin:softwares_list",
         link_text="Softwares",
         buttons=plugin_buttons,
     ),
-)
-
-# Plugin submenu
-
-menu = PluginMenu(
-    label="Softwares",
-    groups=(
-        ('Foo', (item1)),
-    ),
-    icon_class='"mdi mdi-apps-box'
 )
